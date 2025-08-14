7291194fe5fdad9508f56310e764a6b2f46dabbd
User <giriprasad51001@gmail.com>
2025-08-14 15:35:13 +0530

    ARD  added

--- ARD/layers.py
+++ ARD/layers.py
@@ -2,6 +2,9 @@ import torch
 import torch.nn as nn
 from typing import Callable, Optional, Union
 
+import functools
+import operator
+
 from transformers.activations import ACT2FN
 from transformers.pytorch_utils import Conv1D
 from transformers.modeling_utils import ALL_ATTENTION_FUNCTIONS
@@ -147,4 +150,202 @@ class GPT2BlockARD(GPT2Block):
         super().__init__(config, layer_idx=layer_idx)
         self.attn = GPT2AttentionARD(config, layer_idx=layer_idx)
         self.mlp = GPT2MLPARD(4 * config.n_embd, config)
+
+
+
+class LinearARD(torch.nn.Module):
+    def __init__(self, in_features, out_features, bias=True, thresh=3, ard_init=-10):
+        super(LinearARD, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = torch.nn.Parameter(torch.Tensor(out_features, in_features))
+        self.thresh = thresh
+        if bias:
+            self.bias = torch.nn.Parameter(torch.Tensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+        self.ard_init = ard_init
+        self.log_sigma2 = torch.nn.Parameter(torch.Tensor(out_features, in_features))
+        self.reset_parameters()
+
+    def forward(self, input):
+        if self.training:
+            W_mu = torch.nn.functional.linear(input, self.weight)
+            std_w = torch.exp(self.log_alpha).permute(1,0)
+            W_std = torch.sqrt((input.pow(2)).matmul(std_w*(self.weight.permute(1,0)**2)) + 1e-15)
+
+            epsilon = W_std.new(W_std.shape).normal_()
+            output = W_mu + W_std * epsilon
+            if self.bias is not None : output += self.bias
+        else:
+            W = self.weights_clipped
+            output = torch.nn.functional.linear(input, W) + self.bias
+        return output
+
+    @property
+    def weights_clipped(self):
+        clip_mask = self.get_clip_mask()
+        return torch.where(clip_mask, torch.zeros_like(self.weight), self.weight)
+
+    def reset_parameters(self):
+        self.weight.data.normal_(0, 0.02)
+        if self.bias is not None:
+            self.bias.data.zero_()
+        self.log_sigma2.data.fill_(self.ard_init)
+
+    def get_clip_mask(self):
+        log_alpha = self.log_alpha
+        return torch.ge(log_alpha, self.thresh)
+
+    def get_reg(self, **kwargs):
+        """
+        Get weights regularization (KL(q(w)||p(w)) approximation)
+        """
+        k1, k2, k3 = 0.63576, 1.8732, 1.48695
+        C = -k1
+        mdkl = k1 * torch.sigmoid(k2 + k3 * self.log_alpha) - \
+            0.5 * torch.log1p(torch.exp(-self.log_alpha)) + C
+        return -torch.sum(mdkl)
+
+    def extra_repr(self):
+        return 'in_features={}, out_features={}, bias={}'.format(
+            self.in_features, self.out_features, self.bias is not None
+        )
+
+    def get_dropped_params_cnt(self):
+        """
+        Get number of dropped weights (with log alpha greater than "thresh" parameter)
+        :returns (number of dropped weights, number of all weight)
+        """
+        return self.get_clip_mask().sum().cpu().numpy()
+
+    @property
+    def log_alpha(self):
+        log_alpha = self.log_sigma2 - 2 * \
+            torch.log(torch.abs(self.weight) + 1e-15)
+        return torch.clamp(log_alpha, -10, 10)
+
+
+class Conv2dARD(torch.nn.Conv2d):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
+                 padding=0, dilation=1, groups=1, ard_init=-10, thresh=3):
+        bias = False  # Goes to nan if bias = True
+        super(Conv2dARD, self).__init__(in_channels, out_channels, kernel_size, stride,
+                                        padding, dilation, groups, bias)
+        self.bias = None
+        self.thresh = thresh
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.ard_init = ard_init
+        self.log_sigma2 = torch.nn.Parameter(ard_init * torch.ones_like(self.weight))
+        # self.log_sigma2 = Parameter(2 * torch.log(torch.abs(self.weight) + eps).clone().detach()+ard_init*torch.ones_like(self.weight))
+
+    def forward(self, input):
+        """
+        Forward with all regularized connections and random activations (Beyesian mode). Typically used for train
+        """
+        if self.training == False:
+            return torch.nn.functional.conv2d(input, self.weights_clipped,
+                            self.bias, self.stride,
+                            self.padding, self.dilation, self.groups)
+        W = self.weight
+
+        conved_mu = torch.nn.functional.conv2d(input, W, self.bias, self.stride,
+                             self.padding, self.dilation, self.groups)
+        log_alpha = self.log_alpha
+        conved_si = torch.sqrt(1e-15 + torch.nn.functional.conv2d(input * input,
+                                                torch.exp(log_alpha) * W *
+                                                W, self.bias, self.stride,
+                                                self.padding, self.dilation, self.groups))
+        conved = conved_mu + \
+            conved_si * \
+            torch.normal(torch.zeros_like(conved_mu),
+                         torch.ones_like(conved_mu))
+        return conved
+
+    @property
+    def weights_clipped(self):
+        clip_mask = self.get_clip_mask()
+        return torch.where(clip_mask, torch.zeros_like(self.weight), self.weight)
+
+    def get_clip_mask(self):
+        log_alpha = self.log_alpha
+        return torch.ge(log_alpha, self.thresh)
+
+    def get_reg(self, **kwargs):
+        """
+        Get weights regularization (KL(q(w)||p(w)) approximation)
+        """
+        k1, k2, k3 = 0.63576, 1.8732, 1.48695
+        C = -k1
+        log_alpha = self.log_alpha
+        mdkl = k1 * torch.sigmoid(k2 + k3 * log_alpha) - \
+            0.5 * torch.log1p(torch.exp(-log_alpha)) + C
+        return -torch.sum(mdkl)
+
+    def extra_repr(self):
+        return 'in_features={}, out_features={}, bias={}'.format(
+            self.in_channels, self.out_channels, self.bias is not None
+        )
+
+    def get_dropped_params_cnt(self):
+        """
+        Get number of dropped weights (greater than "thresh" parameter)
+
+        :returns (number of dropped weights, number of all weight)
+        """
+        return self.get_clip_mask().sum().cpu().numpy()
+
+    @property
+    def log_alpha(self):
+        log_alpha = self.log_sigma2 - 2 * \
+            torch.log(torch.abs(self.weight) + 1e-15)
+        return torch.clamp(log_alpha, -8, 8)
+
+
+class ELBOLoss(torch.nn.Module):
+    def __init__(self, net, loss_fn):
+        super(ELBOLoss, self).__init__()
+        self.loss_fn = loss_fn
+        self.net = net
+
+    def forward(self, input, target, loss_weight=1., kl_weight=1.):
+        assert not target.requires_grad
+        # Estimate ELBO
+        return loss_weight * self.loss_fn(input, target)  \
+            + kl_weight * get_ard_reg(self.net)
+
+
+def get_ard_reg(module):
+    """
+    :param module: model to evaluate ard regularization for
+    :param reg: auxilary cumulative variable for recursion
+    :return: total regularization for module
+    """
+    if isinstance(module, LinearARD) or isinstance(module, Conv2dARD):
+        return module.get_reg()
+    elif hasattr(module, 'children'):
+        return sum([get_ard_reg(submodule) for submodule in module.children()])
+    return 0
+
+
+def _get_dropped_params_cnt(module):
+    if hasattr(module, 'get_dropped_params_cnt'):
+        return module.get_dropped_params_cnt()
+    elif hasattr(module, 'children'):
+        return sum([_get_dropped_params_cnt(submodule) for submodule in module.children()])
+    return 0
+
+
+def _get_params_cnt(module):
+    if any([isinstance(module, l) for l in [LinearARD, Conv2dARD]]):
+        return functools.reduce(operator.mul, module.weight.shape, 1)
+    elif hasattr(module, 'children'):
+        return sum(
+            [_get_params_cnt(submodule) for submodule in module.children()])
+    return sum(p.numel() for p in module.parameters())
+
+
+def get_dropped_params_ratio(model):
+    return _get_dropped_params_cnt(model) * 1.0 / _get_params_cnt(model)
 
